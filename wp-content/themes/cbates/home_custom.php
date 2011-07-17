e954d90db182ab98503ff22d7d08fcff334ae566
Peng Zhong <nylira@majuro.dreamhost.com>
2011-07-17 18:14:35 -0400

    homepage update

--- wp-content/themes/cbates/home_custom.php
+++ wp-content/themes/cbates/home_custom.php
@@ -2,13 +2,21 @@
 
 <?php get_header(); ?>
 <div id="home_custom">
-<h1>
+<h1 class="logo">
   <span class="name">Cooper Bates</span>
   <span class="subheading">Photography</span>
 </h1>
 <ul>
-  <li class="journal"><a href="<?php bloginfo('url'); ?>/journal/">Journal</a>
-  <li class="portfolio"><a href="<?php bloginfo('url'); ?>/portfolio/weddings/shuyang-felix/">Portfolio</a>
+  <li class="portfolio">
+    <a href="<?php bloginfo('url'); ?>/portfolio/weddings/shuyang-felix/">
+      <img src="<?php bloginfo('template_directory'); ?>/img/couple.jpg">
+      <span>Portfolio</span>
+    </a>
+  <li class="journal">
+    <a href="http://cooperbatesphotography.wordpress.com/">
+      <img src="<?php bloginfo('template_directory'); ?>/img/car.jpg">
+      <span>Journal</span>
+    </a>
 </ul>
 
 </div>
